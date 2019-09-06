026eb200d4d76a6b4665d5a937abcfbcc8f330b9
Jeremy Ling <jlzc89@gmail.com>
2019-09-05 22:16:15 -0700

    removed pods

--- Pods/Target Support Files/Pods-Acorn/Pods-Acorn-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Acorn : NSObject
-@end
-@implementation PodsDummy_Pods_Acorn
-@end
--- Pods/Target Support Files/Pods-AcornTests/Pods-AcornTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AcornTests : NSObject
-@end
-@implementation PodsDummy_Pods_AcornTests
-@end
